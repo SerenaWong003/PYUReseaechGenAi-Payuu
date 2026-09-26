6195e2c4ebf7b0dd6a6e74d2604fd9389a3da7dd
SerenaWong003 <47673211+SerenaWong003@users.noreply.github.com>
2026-09-26 13:10:52 +0700

    Update PaYuuAi.py

--- PaYuuAi.py
+++ PaYuuAi.py
@@ -41,8 +41,8 @@ HF_MODELS = {
     "Gorilla (เขียนโค้ด/API)": "gorilla-llm/gorilla-7b-hf-v0",
     "ChatGLM (อ่านบริบทยาว/Long context)": "THUDM/chatglm3-6b",}
 GEMINI_MODEL_MAP = {
-    "Gemini 1.5 Flash": "gemini-1.5-flash-latest",
-    "Google Gemini 1.5 Pro": "gemini-1.5-pro-latest",}
+    "Gemini 1.5 Flash": "gemini-1.5-flash",
+    "Google Gemini 1.5 Pro": "gemini-1.5-pro",}
 
 
 # ==========================================
