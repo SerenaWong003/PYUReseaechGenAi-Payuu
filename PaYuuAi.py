68446da9ab92f08d47c9838ccc961b28281ca056
SerenaWong003 <47673211+SerenaWong003@users.noreply.github.com>
2026-09-26 09:19:22 +0700

    Update PaYuuAi.py

--- PaYuuAi.py
+++ PaYuuAi.py
@@ -12,8 +12,7 @@ from datetime import datetime
 from urllib.parse import quote
 import google.generativeai as genai
 
-function onSignIn(googleUser) {
-  var profile = googleUser.getBasicProfile();
+
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
