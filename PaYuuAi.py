ce91fcd537b5b32d6c387dd9f1879599da874585
SerenaWong003 <47673211+SerenaWong003@users.noreply.github.com>
2026-09-26 09:18:28 +0700

    Update PaYuuAi.py

--- PaYuuAi.py
+++ PaYuuAi.py
@@ -12,7 +12,6 @@ from datetime import datetime
 from urllib.parse import quote
 import google.generativeai as genai
 
-<div class="g-signin2" data-onsuccess="onSignIn"></div>
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
